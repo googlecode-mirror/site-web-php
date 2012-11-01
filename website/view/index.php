aa180d1c2d69e22c2a8fee8926d25bff81b86084
Malika <Malika@Malika-Laptop>
2012-11-01 18:07:29 +0100

    initial commit

---
+++ website/view/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>index</title>
+<LINK href="./style/stylesheet.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="wrapper">
+	 <h1>Echo</h1>
+	 <ul>
+   <li><a href="#1">Home</a></li>
+   <li><a href="#2">Portfolio</a></li>
+   <li><a href="#3">News</a></li>
+   <li><a href="#4">Labs</a></li>
+   <li><a href="#5">Contact</a></li>
+	</ul>
+	 
+	 
+	 
+	<?php
+	 echo ('PHP works !');
+	 ?>
+	 aazeaze
+</div>
+
+ </body>
